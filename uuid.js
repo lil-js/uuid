eb80cc12778841c6ede1f0dec108b73fd9873829
Tomas Aparicio <tomas@aparicio.me>
2014-09-14 14:12:58 +0200

    chore: normalize quotes

--- uuid.js
+++ uuid.js
@@ -20,10 +20,10 @@
   }
 
   function uuid() {
-    var uuid = "", i, random
+    var uuid = '', i, random
     for (i = 0; i < 32; i++) {
       random = Math.random() * 16 | 0;
-      if (i === 8 || i === 12 || i === 16 || i === 20) uuid += "-"
+      if (i === 8 || i === 12 || i === 16 || i === 20) uuid += '-'
       uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16)
     }
     return uuid
