a89168d06e69ce3949c90568e1e422188d77b28b
Tomas Aparicio <tomas@aparicio.me>
2014-09-16 20:53:30 +0200

    chore(test): add better coverage

--- test/uuid.js
+++ test/uuid.js
@@ -10,8 +10,28 @@ describe('uuid', function () {
     expect(uuid.isUUID(uuid(), 4)).to.be.true
   })
 
+  it('should be a valid UUID version 4', function () {
+    expect(uuid.isUUID('9c858901-8a57-4791-81fe-4c455b099bc9', 4)).to.be.true
+  })
+
+  it('should not be a valid UUID version 4', function () {
+    expect(uuid.isUUID('A987FBC9-4BED-3078-CF07-9141BA07C9F3', 4)).to.be.false
+  })
+
   it('should be a valid UUID version 3', function () {
-    expect(uuid.isUUID('4482e285-3f19-40b7-8084-308f08ccf6ab')).to.be.true
+    expect(uuid.isUUID('A987FBC9-4BED-3078-CF07-9141BA07C9F3', 3)).to.be.true
+  })
+
+  it('should be a valid UUID version 5', function () {
+    expect(uuid.isUUID('987FBC97-4BED-5078-BF07-9141BA07C9F3', 5)).to.be.true
+  })
+
+  it('should not be a valid UUID version 3', function () {
+    expect(uuid.isUUID('987FBC97-4BED-5078-BF07-9141BA07C9F3', 3)).to.be.false
+  })
+
+  it('should be a valid UUID version 4', function () {
+    expect(uuid.isUUID('987FBC97-4BED-5078-BF07-9141BA07C9F3', 4)).to.be.false
   })
 
   it('should be a valid UUID without specifying version', function () {
@@ -19,7 +39,7 @@ describe('uuid', function () {
   })
 
   it('should not be a valid UUID version 5', function () {
-    expect(uuid.isUUID('invalid')).to.be.false
+    expect(uuid.isUUID('xxxA987FBC9-4BED-3078-CF07-9141BA07C9F3')).to.be.false
     expect(uuid.isUUID(null)).to.be.false
     expect(uuid.isUUID(undefined)).to.be.false
     expect(uuid.isUUID({})).to.be.false
